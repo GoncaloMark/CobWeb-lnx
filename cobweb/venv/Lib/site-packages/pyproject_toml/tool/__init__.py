95b5e15997da2f2ce3b81014389f682c00d05142
GoncaloMark <gmgoncalo7@gmail.com>
2023-04-24 21:52:59 +0100

    Optimazation of methods

--- cobweb/venv/Lib/site-packages/pyproject_toml/tool/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import pyproject_toml
-
-__schema__ = {
-    "type": "object",
-    "properties": {"pyproject-toml": pyproject_toml.__schema__},
-}
--- cobweb/venv/Lib/site-packages/pyproject_toml/tool/pyproject_toml.py
+++
@@ -1,8 +0,0 @@
-__schema__ = {
-    "type": "object",
-    "properties": {
-        "packages": {
-            "$ref": "http://json-schema.org/draft-07/schema#/definitions/stringArray",
-        }
-    },
-}
